ce60b3a36b002c9f6a8604f5075d2a2532b4fa3b
Lucy Mason <87684856+Queen-O-Hearts@users.noreply.github.com>
2022-08-10 20:27:08 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            title: 'Lucyyy',
+            viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+            charset: 'utf-8',
+            meta: [ { name: 'description', content: 'Hi there!' } ],
+            bodyAttrs: { class: 'test' }
+        }
+    }
 })
